dee802ff6d3921d0e1e2ca69ec756ab38c81a814
Yuka Maeno <yuka_imato@yahoo.co.jp>
2018-01-08 07:28:16 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :new, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -11,11 +14,11 @@ class TasksController < ApplicationController
   # new ... 新規登録をするためのフォームを表示するだけ
   # create ... 実際に登録をするので、画面を持たない
   def new
-     @task = current_user.tasks.new
+     @task = Task.new
   end
 
   def create
-    @task = current_user.tasks.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -58,5 +61,22 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @taskst
+      redirect_to root_url
+    end
+  end
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def counts(user)
+    @count_tasks = user.tasks.count
   end
 end
